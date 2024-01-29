39dd9ae5afa0e415add356ae9a6598da769a70f4
mirocsd <mirostrasz@gmail.com>
2024-01-28 22:22:09 -0700

    Corrected bar for single monitor

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -13,7 +13,7 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 screens=$(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f6)
 
 if [[ $(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f4 | cut -d"+" -f2- | uniq | wc -l) == 1 ]]; then
-  MONITOR=$(polybar --list-monitors | cut -d":" -f1) TRAY_POS=right polybar miro &
+  MONITOR=$(polybar --list-monitors | cut -d":" -f1) TRAY_POS=right polybar mirobar &
 else
 	primary=$(xrandr --query | grep primary | cut -d" " -f1)
 
